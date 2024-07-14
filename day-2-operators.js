19423895be01dc62fea8b4d5d7f24729d2b9ddc5
himanshu bokde <himanshubokde15@gmail.com>
2024-07-14 17:05:29 +0530

    day -2-Operators

---
+++ day-2-operators.js
@@ -0,0 +1,67 @@
+// day 2: Operators
+
+// Activity 1: Arithmatic Operstions
+
+
+let a = 10
+let b = 10
+
+console.log("addition", a + b); // 20
+
+console.log("substraction", a - b); // 0
+
+console.log("multiplication", a * b); // 100
+
+console.log("division", a / b); // 1
+
+console.log("remainder", a % b); // 0
+
+
+// Activity 2: Assignment OPerators
+
+a += 20
+console.log("value added:", a); // 30
+a -= 10
+console.log("value subtracted:", a); // 20
+
+// Activity 3: Comparison Operators
+
+a = 20, b = 10
+console.log(a < b); // false 
+
+console.log(a > b); // true
+
+console.log(a <= b); // false
+
+console.log(a >= b); // true
+
+console.log(a == b); // false
+
+console.log(a === b); // false
+
+
+// Activity 4: Logical Operators
+
+if (true && false) {
+    console.log("both must be true: true");
+} else {
+    console.log("if both are not true : else statement run");
+}
+
+if (true || false) {
+    console.log("only one should true: true");
+}
+
+if(2!==5){
+    console.log(true);
+}
+
+// Activity 5: Ternery Operators
+
+let x = -10
+if (x > 0 ? console.log("positive") : console.log("negative"));
+
+// explaning : if x is greater than 0 then value is positive else value will be negative
+
+
+//+++++++++++    day 2 has completed 🥳🎈    ++++++++++++
